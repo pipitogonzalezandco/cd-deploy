ac1a556c503a63695779a59b4c14d8b57268ef95
pipitogonzalezandco <pipitogonzalezandco@gmail.com>
2024-11-19 18:13:54 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <p>Developed by Pipito Gonzalez 🥳</p>
+      <p>And hoster.kz</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
